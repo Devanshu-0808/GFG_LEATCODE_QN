a607c532088cdf1668cb51400460a38acb30a7e9
Devanshu Chatterjee <126085261+Devanshu-0808@users.noreply.github.com>
2024-03-12 00:35:57 +0530

    Create Count pairs Sum in matrices.cpp

---
+++ Count pairs Sum in matrices.cpp
@@ -0,0 +1,31 @@
+	int countPairs(vector<vector<int>> &mat1, vector<vector<int>> &mat2, int n, int x)
+	{
+	  int row1=0 , row2=n-1 , col1=0,col2=n-1 , count=0;
+	  while(row1<n && row2>=0)
+	  {
+	      if(mat1[row1][col1]+mat2[row2][col2] ==x)
+	      {
+	          col1++;
+	          col2--;
+	          count++;
+	      }
+	      else if( x> mat1[row1][col1]+mat2[row2][col2])
+	      {
+
+	          col1++;
+	      }
+	      else if(x< mat1[row1][col1]+mat2[row2][col2])
+	      {
+	          col2--;
+	      }
+	      if(col1==n) {
+	          row1++;
+	          col1=0;
+	      }
+	      if(col2==-1){
+	          col2=n-1;
+	          row2--;
+	      }
+	  }
+	  return count;
+	}
