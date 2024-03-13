06678d9f273ed9dc14b525554d95a966fc896d64
Devanshu Chatterjee <126085261+Devanshu-0808@users.noreply.github.com>
2024-03-13 07:19:33 +0530

    LEETCODE

---
+++ Reverse String II.cpp
@@ -0,0 +1,15 @@
+string reverseStr(string s, int k) {
+    
+       
+      for(int i=0 ; i<s.size() ; i=i+(2*k))
+      {
+           if(s.size()-i<k)
+           {
+            reverse(s.begin()+i , s.end());
+           }
+           else
+           {
+            reverse(s.begin()+i , s.begin()+i+k);
+           }
+      }
+        return s;
